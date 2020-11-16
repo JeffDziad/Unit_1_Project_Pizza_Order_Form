21a15661900c1d40b863a1da0c21ce4a6251552e
BuildTools <unconfigured@null.spigotmc.org>
2020-11-15 21:32:50 -0600

    Final Changes Before Turn In

--- code.js
+++ code.js
@@ -1,10 +1,8 @@
 $(document).ready(function (){
     $("#orderNow").click(function (){
        window.location = 'order.html';
-        window.addEventListener("load", function() {
-            alert('Page is loaded');
-        });
     });
+
 
     $("#pizzaSizeLabel").click(function() {
         if ($("#pizzaSizeContent").is(":visible"))      //IF only size content is visible
@@ -85,5 +83,63 @@ $(document).ready(function (){
             $("#pizzaVeggieLabel").toggleClass("rounded");
         }
     });
+
+    $("#confirmOrderTab").click(sendToOrderSummary);
+
+
+    function sendToOrderSummary()
+    {
+        var firstName = $("#firstName").val();
+        var lastName = $("#lastName").val();
+        var streetAddress = $("#streetAdd").val();
+        var phone = $("#phone").val();
+
+        var pizzaSize = $("input[name=pizzaSize]:checked").val();
+        if (pizzaSize === undefined)
+        {
+            pizzaSize = "None Selected";
+        }
+        var pizzaCrust = $("input[name=pizzaCrust]:checked").val();
+        if (pizzaCrust === undefined)
+        {
+            pizzaCrust = "None Selected"
+        }
+        var pizzaToppingsMeat = [];
+        var pizzaToppingsVeggie = [];
+        $("input[name=meats]:checked").each(function() {
+            pizzaToppingsMeat.push($(this).val())
+        });
+        $("input[name=veggies]:checked").each(function() {
+            pizzaToppingsVeggie.push($(this).val());
+        });
+        var pizzaSizeCost = 0;
+        var pizzaToppingsMeatCost = pizzaToppingsMeat.length * 1.50;
+        var pizzaToppingsVeggieCost = pizzaToppingsVeggie.length;
+
+        switch (pizzaSize)
+        {
+            case "Small":
+                pizzaSizeCost = 7;
+                break;
+            case "Medium":
+                pizzaSizeCost = 9;
+                break;
+            case "Large":
+                pizzaSizeCost = 12;
+                break;
+        }
+
+        var subTotal = pizzaSizeCost + pizzaToppingsMeatCost + pizzaToppingsVeggieCost;
+
+        var taxAmt = subTotal * 0.051;
+
+        var deliveryFee = 2;
+
+        var total = subTotal + taxAmt + deliveryFee;
+
+        $("#outputID").text(`Customer Information:\n\nFirst Name: ${firstName}\nLast Name: ${lastName}\nAddress: ${streetAddress}\nPhone Number: ${phone}`);
+        $("#output").text(`Pizza Size: ${pizzaSize}\nPizza Crust: ${pizzaCrust}\nMeat Toppings: ${pizzaToppingsMeat}\nVeggie Toppings: ${pizzaToppingsVeggie}\n\nSubtotal: $${subTotal}\nTax: $${taxAmt.toFixed(2)}\nDelivery Fee: $${deliveryFee}\n\nTotal: $${total.toFixed(2)}`);
+
+    }
 
 });
